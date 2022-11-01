9530ade7c3416b7aef4fb48f2344028b169e08c1
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-02 00:23:30 +0100

    Create first Hello World in c++

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    std::cout << "Hello World!" << std::endl;
 }
-
-/* C++ Template Project */
