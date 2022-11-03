19e689ac193a578fa7d90d328698d39ec99e7266
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-03 23:49:29 +0100

    Show the case of the possible Run Time Error

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
 int main(){
+    
+    // Compile Time Error
     std::cout << "Hello World!" << std::endl;
+
+    // Run Time Error
+    int value = 7/0;
+    std::cout << "value :" << value << std::endl;
+
+    return 0;
 }
