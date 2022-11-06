d5ea917a955924ff62e367c3d96a04099f812333
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-06 17:49:25 +0100

    Extend functionality to enter full name

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 int main(){
 
     int age;
-    std::string name;
+    std::string full_name;
 
-    std::cout << "Type your name: " << std::endl;
-    std::cin >> name;
+    std::cout << "Type your full name: " << std::endl;
+    std::getline(std::cin, full_name);
 
     std::cout << "Type your age: " << std::endl;
     std::cin >> age;
 
-    std::cout << "Hello " << name << "! You are " << age << " years old." << std::endl;
+    std::cout << "Hello " << full_name << "! You are " << age << " years old." << std::endl;
 
     return 0;   
 }
