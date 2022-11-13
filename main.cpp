23c5b2cab3a69c1740f2d990e0f0f810aaef7a84
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-13 23:22:48 +0100

    Different notation of appending integers to an array

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ int main(){
 
     int scores[10];
 
-    for (size_t i{0}; i < 10; i++) {
-        scores[i] = i * 3;
+    for (int i{0}; i < 10; i++) {
+        scores[i] = i * i;
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (int i{0}; i < 10; i++) {
         std::cout << scores[i] << std::endl; 
     }
 
